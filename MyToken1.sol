5b49ec4b0ca2d33892854148f3f7ba4549cfad27
mawang666888 <mawang666888@gmail.com>
2025-12-20 21:46:59 +0800

    tj

---
+++ MyToken1.sol
@@ -0,0 +1,7 @@
+//方式一
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+//还可以指定版本号
+//import "@openzeppelin/contracts@4.9.2/utils/cryptography/ECDSA.sol";
+
+//方式二
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/governance/Governor.sol";
